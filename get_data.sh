f305a9949e0cfe6182dcf6beacebd99bd1bc1c11
Wenhu Chen <wenhuchen@ucsb.edu>
2019-11-24 00:24:48 -0800

    update

---
+++ get_data.sh
@@ -0,0 +1,6 @@
+wget https://gqadata.s3-us-west-2.amazonaws.com/questions.zip
+unzip questions.zip
+wget https://gqadata.s3-us-west-2.amazonaws.com/scene_graph.zip
+unzip scene_graph.zip
+mkdir sceneGraphs
+mv trainval_bounding_box.json sceneGraphs
