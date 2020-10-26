5107ffcf0bfc3eac559d283e1dbc07353b6271f1
wenhu chen <wenhuchen@cs.ucsb.edu>
2020-10-26 13:50:53 -0700

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -4,3 +4,5 @@ wget https://gqadata.s3-us-west-2.amazonaws.com/scene_graph.zip
 unzip scene_graph.zip
 mkdir sceneGraphs
 mv trainval_bounding_box.json sceneGraphs
+wget https://convaisharables.blob.core.windows.net/meta-module-network/gqa_visual_features.zip
+unzip gqa_visual_features.zip
